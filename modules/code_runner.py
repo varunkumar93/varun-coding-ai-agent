6aacb2e65c237127e74524a02206b29d8efa65e0
varunkumar93 <63653758+varunkumar93@users.noreply.github.com>
2025-08-17 21:02:30 +0530

    Create code_runner.py

---
+++ modules/code_runner.py
@@ -0,0 +1,12 @@
+import io
+import contextlib
+
+class CodeRunner:
+    def run_code(self, code):
+        output = io.StringIO()
+        try:
+            with contextlib.redirect_stdout(output):
+                exec(code, {})
+            return output.getvalue()
+        except Exception as e:
+            return str(e)
