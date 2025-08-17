1282f4ee8474d09307b87ff9b3c03ea69f20503b
varunkumar93 <63653758+varunkumar93@users.noreply.github.com>
2025-08-17 20:43:44 +0530

    Create learning_path.py

---
+++ modules/learning_path.py
@@ -0,0 +1,15 @@
+class LearningPath:
+    def __init__(self):
+        self.paths = {
+            "Beginner": ["Variables", "Loops", "Functions"],
+            "Debug Master": ["Tracebacks", "Common Errors", "Fix Strategies"],
+            "Groq Pro": ["Setup", "Prompting", "Streaming Responses"]
+        }
+
+    def recommend(self, progress):
+        # Simple logic: recommend next uncompleted lesson
+        for path, lessons in self.paths.items():
+            for lesson in lessons:
+                if lesson not in progress.values():
+                    return path, lesson
+        return "All done!", None
