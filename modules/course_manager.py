a2fb1713cdab0d1f47dab1c77c2d98bb4ce4fc1b
varunkumar93 <63653758+varunkumar93@users.noreply.github.com>
2025-08-18 00:23:22 +0530

    Update course_manager.py

--- modules/course_manager.py
+++ modules/course_manager.py
@@ -1,7 +1,7 @@
 class ProgramizCourseManager:  # You can rename this to FreeCodeCampCourseManager if you prefer
     def get_lesson_content(self, language, topic_slug):
         if topic_slug == "variables-data-types":
-            return """
+            return """\
 ### 📘 Variables and Data Types (freeCodeCamp)
 Variables store data. Data types define the kind of data.
 
